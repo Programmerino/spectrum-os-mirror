79b692c4e5b70fe974344677d7092b7318bfa8f9
Alyssa Ross <alyssa.ross@unikie.com>
2022-09-30 21:09:04 +0000

    cloud-hypervisor: add virtio-gpu support
    The virtio-bindings changes update the bindings for recent kernels,
    and the vhost change is cherry-picked from crosvm's fork of the crate
    to add support for their custom extensions.
    
    Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- pkgs/applications/virtualization/cloud-hypervisor/default.nix
+++ pkgs/applications/virtualization/cloud-hypervisor/default.nix
@@ -22,12 +22,43 @@ rustPlatform.buildRustPackage rec {
       "versionize_derive-0.1.4" = "sha256-oGuREJ5+FDs8ihmv99WmjIPpL2oPdOr4REk6+7cV/7o=";
       "vfio-bindings-0.4.0" = "sha256-8zdpLD9e1TAwG+m6ifS7/Fh39fAs5VxtnS5gUj/eKmY=";
       "vfio_user-0.1.0" = "sha256-b/gL6vPMW44O44lBIjqS+hgqVUUskBmttGk5UKIMgZk=";
-      "vhost-0.7.0" = "sha256-KdVROh44UzZJqtzxfM6gwAokzY6El8iDPfw2nnkmhiQ=";
       "vm-fdt-0.2.0" = "sha256-lKW4ZUraHomSDyxgNlD5qTaBTZqM0Fwhhh/08yhrjyE=";
     };
   };
 
   separateDebugInfo = true;
+
+  vhost = fetchFromGitHub {
+    name = "vhost";
+    owner = "rust-vmm";
+    repo = "vhost";
+    rev = "bdc6f2ab2b3dbd3b9574100ac641a2f8e9667400";
+    hash = "sha256-p58Jty+GpRFOO9+YcAnDtAAOYi19+7I6FgvnHZZTj0w=";
+  };
+
+  postUnpack = ''
+    unpackFile ${vhost}
+    chmod -R +w vhost
+  '';
+
+  cargoPatches = [
+    ./0001-build-use-local-vhost.patch
+    ./0002-virtio-devices-add-a-GPU-device.patch
+  ];
+
+  vhostPatches = [
+    vhost/0001-vhost_user-add-shared-memory-region-support.patch
+    vhost/0002-devices-vhost-user-add-protocol-flag-for-shmem.patch
+  ];
+
+  postPatch = ''
+    pushd ../vhost
+    for patch in $vhostPatches; do
+        echo applying patch $patch
+        patch -p1 < $patch
+    done
+    popd
+  '';
 
   nativeBuildInputs = [ pkg-config ];
   buildInputs = [ openssl ] ++ lib.optional stdenv.isAarch64 dtc;
